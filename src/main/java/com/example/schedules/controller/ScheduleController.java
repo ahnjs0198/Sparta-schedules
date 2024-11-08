04426b5f8dc32e543a1ed3661db84377223c5ad5
ahnjs0198 <ahnjs0198@gmail.com>
2024-11-08 21:59:15 +0900

    Feat: 일정 전체 조회 기능 추가

--- src/main/java/com/example/schedules/controller/ScheduleController.java
+++ src/main/java/com/example/schedules/controller/ScheduleController.java
@@ -3,12 +3,12 @@ package com.example.schedules.controller;
 import com.example.schedules.dto.ScheduleRequestDto;
 import com.example.schedules.dto.ScheduleResponseDto;
 import com.example.schedules.entity.Schedule;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/schedules")
@@ -17,15 +17,33 @@ public class ScheduleController {
     private final Map<Long, Schedule> scheduleList = new HashMap<>();
 
     @PostMapping
-    public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto dto) {
+    public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto dto) {
 
+        // ScheduleId 식별자 계산
         Long scheduleId = scheduleList.isEmpty() ? 1 : Collections.max(scheduleList.keySet()) + 1;
 
+        // 요청받은 데이터로 Schedule 객체 생성
         Schedule schedule = new Schedule(scheduleId, dto.getTodo(), dto.getWriter(), dto.getPassword(), LocalDateTime.now(), LocalDateTime.now());
 
+        // Inmemory DB에 Schedule 저장
         scheduleList.put(scheduleId, schedule);
 
-        return new ScheduleResponseDto(schedule);
+        return new ResponseEntity<>(new ScheduleResponseDto(schedule), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ScheduleResponseDto>> findAllSchedules() {
+
+        // init List
+        List<ScheduleResponseDto> responseList = new ArrayList<>();
+
+        // HashMap<Schedule> -> List<ScheduleResponseDto>
+        for (Schedule schedule : scheduleList.values()) {
+            ScheduleResponseDto responseDto = new ScheduleResponseDto(schedule);
+            responseList.add(responseDto);
+        }
+
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
